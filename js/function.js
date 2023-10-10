8ea5950aafc8fbad9e5ccc01c2753aaf0ffc8f68
이두 <dlgkrwls05@gmail.com>
2023-10-10 17:52:05 +0900

    10월 10일 업로드

--- js/function.js
+++ js/function.js
@@ -1,8 +1,9 @@
 // 기타 이미지 슬라이드
 $(function () {
-	const $indicator = $('.guitar-slides-pagination a');
+	const $indicator = $('.guitar-slides-pagination>li>a');
 	const $container = $('.guitar-slides-container');
 
+	let aniChk = false;
 	let nowIdx = 0;
 
 	const moveFn = function () {
@@ -20,7 +21,16 @@ $(function () {
 		moveFn();
 	});
 
-	let aniChk = false;
+	$indicator.on('click', function (evt) {
+		evt.preventDefault();
+
+		nowIdx = $indicator.index(this);
+		$container.stop().animate({
+			left: -100 * nowIdx + '%',
+		});
+
+		$indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+	});
 
 	$('.slides-next').on('click', function (evt) {
 		evt.preventDefault();
@@ -34,7 +44,10 @@ $(function () {
 				nowIdx = 0;
 			}
 
-			$container.stop().animate({ left: -100 * nowIdx + '%' }, 400, function () {
+			$container.stop().animate({ left: '-100%' }, 400, function () {
+				const $slides = $('.guitar-slides-container>li');
+				$slides.first().appendTo($container);
+				$container.css({ left: 0 });
 				aniChk = !aniChk;
 			});
 
@@ -51,10 +64,14 @@ $(function () {
 			if (nowIdx > 0) {
 				nowIdx--;
 			} else {
-				nowIdx = $indicator.length - 1;
+				nowIdx = 3;
 			}
 
-			$container.stop().animate({ left: -100 * nowIdx + '%' }, 400, 'easeInOutCubic', function () {
+			const $slides = $('.guitar-slides-container>li');
+			$slides.last().prependTo($container);
+			$container.css({ left: '100%' });
+
+			$container.stop().animate({ left: '0%' }, 400, function () {
 				aniChk = !aniChk;
 			});
 
@@ -79,22 +96,31 @@ $(function () {
 
 // 영화 이미지 슬라이드
 $(function () {
-	const $container = $('.movie');
-	const $indicator = $('.movie>div');
-
+	const $container = $('.movieandpeople-slides>.slides-container');
+	const $indicator = $('.movieandpeople-slides>.slides-pagination>li>a');
 	let nowIdx = 0;
 
-	const moveFn = function () {
-		$container.animate({ left: -1230 * nowIdx + 'px' }, 500);
-	};
+	let aniChk2 = false;
 
-	let aniChk = false;
+	const $btnPrev = $('.movieandpeople-slides>.prev');
+	const $btnNext = $('.movieandpeople-slides>.next');
 
-	$('.next').on('click', function (evt) {
+	$indicator.on('click', function (evt) {
 		evt.preventDefault();
 
-		if (!aniChk) {
-			aniChk = !aniChk;
+		nowIdx = $indicator.index(this);
+		$container.stop().animate({
+			left: -100 * nowIdx + '%',
+		});
+
+		$indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+	});
+
+	$btnNext.on('click', function (evt) {
+		evt.preventDefault();
+
+		if (!aniChk2) {
+			aniChk2 = !aniChk2;
 
 			if (nowIdx < $indicator.length - 1) {
 				nowIdx++;
@@ -102,33 +128,35 @@ $(function () {
 				nowIdx = 0;
 			}
 
-			$container.stop().animate({ left: '-100%' }, 400, function () {
-				$container.append($indicator.first());
+			$container.stop().animate({ left: '-98%' }, 400, function () {
+				const $slides = $('.movieandpeople-slides>.slides-container>li');
+				$slides.first().appendTo($container);
 				$container.css({ left: 0 });
-				aniChk = !aniChk;
+				aniChk2 = !aniChk2;
 			});
 
 			$indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
 		}
 	});
 
-	$('.prev').on('click', function (evt) {
+	$btnPrev.on('click', function (evt) {
 		evt.preventDefault();
 
-		if (!aniChk) {
-			aniChk = !aniChk;
+		if (!aniChk2) {
+			aniChk2 = !aniChk2;
 
 			if (nowIdx > 0) {
 				nowIdx--;
 			} else {
-				nowIdx = $indicator.length - 1;
+				nowIdx = 3;
 			}
 
-			$indicator.last().prependTo($container);
-			$container.css({ left: '-100%' });
+			const $slides = $('.movieandpeople-slides>.slides-container>li');
+			$slides.last().prependTo($container);
+			$container.css({ left: '-90%' });
 
-			$container.stop().animate({ left: 0 }, 400, function () {
-				aniChk = !aniChk;
+			$container.stop().animate({ left: '0%' }, 400, function () {
+				aniChk2 = !aniChk2;
 			});
 
 			$indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
